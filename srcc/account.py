b3fd67926bcf75d8bb79b41df2b1857efe2f6c4c
ayumid2025 <bible9683@gmail.com>
2026-03-01 14:01:19 +0300

    Update account.py

--- srcc/account.py
+++ srcc/account.py
@@ -1,1 +1,17 @@
-
+class MarginAccount:
+    def __init__(self, initial_cash, leverage):
+        self.cash = initial_cash
+        self.leverage = leverage
+        self.positions = {}  # symbol -> {shares, entry_price}
+    
+    def buy(self, symbol, price, shares):
+        # To be implemented
+        pass
+    
+    def sell(self, symbol, price, shares):
+        # To be implemented
+        pass
+    
+    def total_value(self, current_prices):
+        # Calculate total equity (cash + position value - debt)
+        pass
